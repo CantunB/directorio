c36011d3f78783e0327fe4d34940a54a74ce0ea1
bernacantun <cantunberna@gmail.com>
2022-06-21 07:20:33 -0500

    actualizaciones herokuÃ

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-const express = require('express');
 const path = require('path');
-
+const express = require('express');
 const app = express();
 
-app.use(express.static('./dist/directorydigital'));
+// Serve static files
+app.use(express.static(__dirname + '/dist/directorydigital'));
 
-app.get('/*', (req,res) => {
-  res.sendFile('index.html', {root: 'dist/directorydigital/'}),
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/directorydigital/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+// default Heroku port
+app.listen(process.env.PORT || 5000);
